9d69cafd3fa77ce73d210aaa17e3918134eb5729
Alex Schmoe <aschmoe@gmail.com>
2020-04-01 15:57:17 -0700

    initial changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,229 @@
+<script context="module">
+  export async function preload({ params }) {
+    console.log("hi2");
+    let res;
+    try {
+      res = await this.fetch(`movies.json`);
+    } catch (e) {
+      this.error(404, "Sorry we couldn't find this entry");
+      return;
+    }
+
+    const data = await res.json();
+
+    if (res.status === 200) {
+      console.log("ummmm");
+      return { options: data };
+    } else {
+      this.error(
+        res.status,
+        data.message || "Sorry we couldn't find this entry"
+      );
+    }
+  }
+</script>
+
+<script>
+  import { onMount } from "svelte";
+
+  export let options = [];
+
+  let didRender = false;
+
+  let startAngle = 0;
+  let arc = Math.PI / (options.length / 2);
+  let spinTimeout = null;
+
+  let spinAngleStart = 10;
+  let spinTime = 0;
+  let spinTimeTotal = 0;
+
+  const canvasWidth = 1000;
+  const canvasHalf = canvasWidth / 2;
+
+  let ctx;
+
+  // document.getElementById("spin").addEventListener("click", spin);
+
+  function byte2Hex(n) {
+    const nybHexString = "0123456789ABCDEF";
+    return (
+      String(nybHexString.substr((n >> 4) & 0x0f, 1)) +
+      nybHexString.substr(n & 0x0f, 1)
+    );
+  }
+
+  function RGB2Color(r, g, b) {
+    return "#" + byte2Hex(r) + byte2Hex(g) + byte2Hex(b);
+  }
+
+  function getColor(item, maxitem) {
+    const phase = 0;
+    const center = 128;
+    const width = 127;
+    const frequency = (Math.PI * 2) / maxitem;
+
+    const red = Math.sin(frequency * item + 2 + phase) * width + center;
+    const green = Math.sin(frequency * item + 0 + phase) * width + center;
+    const blue = Math.sin(frequency * item + 4 + phase) * width + center;
+
+    return RGB2Color(red, green, blue);
+  }
+
+  function drawRouletteWheel() {
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+      const outsideRadius = canvasHalf - 50;
+      const textRadius = canvasHalf - 90;
+      const insideRadius = canvasHalf / 8;
+
+      ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvasWidth, canvasWidth);
+
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+
+      ctx.font = "bold 12px Helvetica, Arial";
+
+      for (let i = 0; i < options.length; i++) {
+        let angle = startAngle + i * arc;
+        //ctx.fillStyle = colors[i];
+        ctx.fillStyle = getColor(i, options.length);
+
+        ctx.beginPath();
+        ctx.arc(
+          canvasHalf,
+          canvasHalf,
+          outsideRadius,
+          angle,
+          angle + arc,
+          false
+        );
+        ctx.arc(canvasHalf, canvasHalf, insideRadius, angle + arc, angle, true);
+        ctx.stroke();
+        ctx.fill();
+
+        ctx.save();
+        // ctx.shadowOffsetX = -1;
+        // ctx.shadowOffsetY = -1;
+        // ctx.shadowBlur = 0;
+        // ctx.shadowColor = "rgb(220,220,220)";
+        ctx.fillStyle = "black";
+        ctx.translate(
+          canvasHalf + Math.cos(angle + arc / 2) * textRadius,
+          canvasHalf + Math.sin(angle + arc / 2) * textRadius
+        );
+        ctx.rotate(angle + arc / 2);
+        // ctx.rotate(angle + arc / 2 + Math.PI / 2);
+        const text = options[i].title;
+        // -ctx.measureText(text).width / 2
+        ctx.fillText(text, -(canvasHalf / 2), 5);
+        ctx.restore();
+      }
+
+      //Arrow
+      ctx.fillStyle = "black";
+      ctx.beginPath();
+      ctx.moveTo(canvasHalf - 4, canvasHalf - (outsideRadius + 5));
+      ctx.lineTo(canvasHalf + 4, canvasHalf - (outsideRadius + 5));
+      ctx.lineTo(canvasHalf + 4, canvasHalf - (outsideRadius - 5));
+      ctx.lineTo(canvasHalf + 9, canvasHalf - (outsideRadius - 5));
+      ctx.lineTo(canvasHalf + 0, canvasHalf - (outsideRadius - 13));
+      ctx.lineTo(canvasHalf - 9, canvasHalf - (outsideRadius - 5));
+      ctx.lineTo(canvasHalf - 4, canvasHalf - (outsideRadius - 5));
+      ctx.lineTo(canvasHalf - 4, canvasHalf - (outsideRadius + 5));
+      ctx.fill();
+    }
+  }
+
+  function rotateWheel() {
+    spinTime += 30;
+    if (spinTime >= spinTimeTotal) {
+      stopRotateWheel();
+      return;
+    }
+    const spinAngle =
+      spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
+    startAngle += (spinAngle * Math.PI) / 180;
+    drawRouletteWheel();
+    spinTimeout = setTimeout(() => rotateWheel(), 30);
+  }
+
+  function stopRotateWheel() {
+    clearTimeout(spinTimeout);
+    const degrees = (startAngle * 180) / Math.PI + 90;
+    const arcd = (arc * 180) / Math.PI;
+    const index = Math.floor((360 - (degrees % 360)) / arcd);
+    ctx.save();
+
+    const background = new Image();
+    background.src = options[index].image;
+    // Make sure the image is loaded first otherwise nothing will draw.
+    background.onload = function() {
+      console.log("loaded", background);
+      const ratio = canvasWidth / background.width;
+      const imageHeight = background.height * ratio;
+      ctx.drawImage(
+        background,
+        0,
+        canvasHalf - imageHeight / 2,
+        canvasWidth,
+        imageHeight
+      );
+      // ctx.drawImage(background, 0, 0);
+      const text = options[index].title;
+      ctx.fillStyle = "white";
+      ctx.shadowOffsetX = 2;
+      ctx.shadowOffsetY = 2;
+      ctx.shadowBlur = 5;
+      ctx.shadowColor = "rgba(20,20,20,0.5)";
+      ctx.font = "bold 50px Helvetica, Arial";
+      ctx.fillText(
+        text,
+        canvasHalf - ctx.measureText(text).width / 2,
+        canvasHalf + 20
+      );
+      ctx.restore();
+    };
+  }
+
+  function spin() {
+    spinAngleStart = Math.random() * 10 + 10;
+    spinTime = 0;
+    spinTimeTotal = Math.random() * 3 + 4 * 1000;
+    rotateWheel();
+  }
+
+  function easeOut(t, b, c, d) {
+    const ts = (t /= d) * t;
+    const tc = ts * t;
+    return b + c * (tc + -3 * ts + 3 * t);
+  }
+
+  onMount(() => {
+    console.log("should render");
+    console.log(options);
+    drawRouletteWheel();
+    // stopRotateWheel();
+  });
+
+  $: optionsValid = options.length > 0;
+</script>
+
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  h1 {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 
-	figure {
+  /* figure {
 		margin: 0 0 1em 0;
 	}
 
@@ -19,6 +231,7 @@
 		width: 100%;
 		max-width: 400px;
 		margin: 0 0 1em 0;
+<<<<<<< HEAD
 	}
 
 	p {
@@ -44,3 +257,57 @@
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+=======
+	} */
+
+  /* p {
+    margin: 1em auto;
+  } */
+
+  .wrapper {
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  input {
+    color: white;
+    text-align: center;
+    cursor: pointer;
+    font-size: 24px;
+    margin: 0 0 0 0;
+    padding: 20px 60px;
+    outline: none;
+    background-color: #777;
+    border: none;
+    border-radius: 5px;
+    box-shadow: 0 9px #95a5a6;
+  }
+
+  input:hover {
+    background-color: #666;
+  }
+
+  input:active {
+    background-color: #666;
+    box-shadow: 0 5px #95a5a6;
+    transform: translateY(4px);
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Bad Movie Night</title>
+</svelte:head>
+
+<div class="wrapper" style="width:{canvasWidth}px">
+  <h1>Bad Movie Night</h1>
+
+  <input on:click={spin} type="button" value="Roll the dice baby!" id="spin" />
+  <canvas id="canvas" width={canvasWidth} height={canvasWidth} />
+</div>
+>>>>>>> initial changes
---
+++ src/routes/movies.json.js
@@ -0,0 +1,256 @@
+export function get(req, res) {
+  const movies = [
+    {
+      title: "50. 'Idle Hands' (1999)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/GxR34Rfliuavl1gzd7IVlqRB3GI=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924425/1_SIAfw3W4n6waF51qtmDisw.0.jpeg"
+    },
+    {
+      title: "49. 'Lionheart' (1990)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/C96ItvCyy5sh4dIyAJTaF40dSms=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924427/1_cMFkPu07uCrifAOu-2g3Dg.0.jpeg"
+    },
+    {
+      title: "48. 'Cellular' (2004)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/Jg6EU0YR8nRAmF77fTlsxmaUTng=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924429/1_dMX5bDRIcmZFue0hQdLP9Q.0.jpeg"
+    },
+    {
+      title: "47. 'Dante's Peak' (1997)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/lnaq6l0yazaJxu-xiVHfra2f9_s=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924431/1_E3efRdYUl7RmioSAJpLR7Q.0.jpeg"
+    },
+    {
+      title: "46. 'Obsessed' (2009)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/xVy9rUIj_ViBvP-BRPXAUGwXW7E=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924433/1_RkrkTJSBqU3iuNj9N_4Amg.0.jpeg"
+    },
+    {
+      title: "45. 'Stone Cold' (1991)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/DXBDcW_Ze5LXB6Mq1ucBV4lwYwM=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924435/1_SJca3HD5gzCgWpB7DoAufg.0.jpeg"
+    },
+    {
+      title: "44. 'The Wicker Man' (2006)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/i3DoOcnjiABYNRVZl_w3e9jwu8c=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924437/1_m5ZJs8AEGkDrG_9InuFZFQ.0.jpeg"
+    },
+    {
+      title: "43. 'Youngblood' (1986)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/2jok8jxp0pB_aPQSM-hjo8BC9aI=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924439/1_R4odYbL7DJt-bFq6BaaDbQ.0.jpeg"
+    },
+    {
+      title: "42. 'Hercules' (2014)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/CpNzD_oX9F6q4XXE3qwOrqtBm8k=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924443/1_WA3mqhLpGZRpjX7s1IT-Ig.0.jpeg"
+    },
+    {
+      title: "41. 'You Got Served' (2004)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/ElMlA3fEfw6QF56Kjwa5Dxv8dRM=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924447/1_7V_XkrUf9DtIXfdv8fyWjw.0.jpeg"
+    },
+    {
+      title: "40. 'Battleship' (2012)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/GVI204TuCClcwOAOWJMPBXproX4=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924451/1_flq2JnM1kt-GqdNAaJOmvw.0.jpeg"
+    },
+    {
+      title: "39. 'Save the Last Dance' (2001)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/--ld7MonlZVoO4az9KaGZoal17s=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924453/1_8eZMu-5RDKaRRa65z-fYlg.0.jpeg"
+    },
+    {
+      title: "38. 'The Fast and the Furious: Tokyo Drift' (2006)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/Dm2gOgkvicsd91E2aAVKiDgucc4=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924455/1_YlZU4CoHVnxaTc_rTZQTGQ.0.jpeg"
+    },
+    {
+      title: "37. 'Just One of the Guys' (1985)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/iuSv0r2aYCAM4LxDAhtkuQUDRZE=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924457/1__loezTA2QW9JrCWvzqZVDQ.0.jpeg"
+    },
+    {
+      title: "36. 'Bad Boys II' (2003)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/qNNUrkmI-r376jKE25IHA1t9_Q8=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924459/1_8iWER53H4k2eVtC9b16ZMA.0.jpeg"
+    },
+    {
+      title: "35. 'Spice World' (1997)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/v1aVpfD0EBZVjTuwL8f5cuRDUyw=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924461/1_Got-298f7Sq_RleYklyDcw.0.jpeg"
+    },
+    {
+      title: "34. 'Battlefield Earth' (2000)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/1Qr8EtVMcYXm_GJlEkhQMQnShP8=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924463/1_ysMN2BX61wYwlYglIilaZA.0.jpeg"
+    },
+    {
+      title: "33. 'Surviving the Game' (1994)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/2weecDG1TGo131IBwgHNQoX6ino=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924467/1_qh-N710GCYqRSw-5fOUEMQ.0.jpeg"
+    },
+    {
+      title: "32. 'Mission: Impossible II' (2000)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/I96Je0tnzaQM2TadjMCWoc533Xk=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924469/1_quASoVon6t8IM_zOEm4PhQ.0.jpeg"
+    },
+    {
+      title: "31. 'Nothing but Trouble' (1991)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/CVB8O2IM8tf0SSX_TiePffNH8r4=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924471/1_I3SrES569brOEnGCwVHRyA.0.jpeg"
+    },
+    {
+      title: "30. 'Demolition Man' (1993)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/XxWlUDtyB25NN0-MAtVN5rZVWTo=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924473/1_nkXmIhQdwT6b8vF1QtOiVA.0.jpeg"
+    },
+    {
+      title: "29. 'The Chase' (1994)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/Jv2cUI6LDWyLwZoZL6t44T54iuQ=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924475/1_FJ0upsGecGdLo5hxFLFrEg.0.jpeg"
+    },
+    {
+      title: "28. 'The Last Action Hero' (1993)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/RFC3YBjlsQIFcLQTAyLGhrptAnE=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924477/1_9MuWfGi3ewx4ZWTEqQpeFQ.0.jpeg"
+    },
+    {
+      title: "27. 'The Happening' (2008)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/h8xXi-2vbz080h5PE34056M9zJ8=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924479/1_JjV631iWVxDANqJVz-jx7w.0.jpeg"
+    },
+    {
+      title: "26. 'Gigli' (2003)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/7ckD9_DNbk_Bs5OvmFEMxBDb0FM=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924481/1_hhd9_TL8cfJVF-FUu1XieQ.0.jpeg"
+    },
+    {
+      title: "25. 'Catwoman' (2004)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/p2hhrDktSMxfDHVOmBiUfKXDc2I=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924483/1_O939bacu-OnsDsGjvPGnDw.0.jpeg"
+    },
+    {
+      title: "24. 'Con Air' (1997)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/9r7zJnareCr9sK7w5rfDRKX6Yps=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924487/1_laAovEJ8ITdLbZT61QNszQ.0.jpeg"
+    },
+    {
+      title: "23. 'The Room' (2003)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/uRbluRcSC4-XGnLdFipaUiUdX3U=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924489/1_S65pOtIGexFXZS09AZvC3w.0.jpeg"
+    },
+    {
+      title: "22. 'Troll 2' (1990)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/qxvUUxZJtS45kJK_FOedIHzAekA=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924491/1_eysADEnlvU0aMw_GFSW9Xg.0.jpeg"
+    },
+    {
+      title: "21. 'Road House' (1989)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/Y3jGf_my_Fh0anFCsad80q41u9s=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924493/1__mXLKRjsQ-yqRjP-bIb86g.0.jpeg"
+    },
+    {
+      title: "20. 'Fear' (1996)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/TyRPKVGSosY1ICEy6aaVLdK263I=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924495/1_ytri6oQ1epXiMgecTdmTXA.0.jpeg"
+    },
+    {
+      title: "19. 'Mortal Kombat' (1995)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/TJZkLir1SUfCxuUvd32NokKuPjU=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924497/1_-L3Cf5vCAmqU3xaK0jTPFg.0.jpeg"
+    },
+    {
+      title: "18. 'Mighty Morphin Power Rangers' (1993)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/OdlV6pKmcnLHG7BeEHxZAGdvbSg=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924501/1_Ci5Rvx8aMF3X5Ek7OT16Zg.0.jpeg"
+    },
+    {
+      title: "17. 'Deep Blue Sea' (1999)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/d3aBeBqXyA0dHVPzUAe6ts4zV_U=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924503/1_g-sfQz5FXocKK0fdtgqJPg.0.jpeg"
+    },
+    {
+      title: "16. 'Honey' (2003)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/8WUvWPP6VF_aNlCBTHA2LNG_Gcs=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924505/1_m9WP3Uf9rguZIoDUL0zBSw.0.jpeg"
+    },
+    {
+      title: "15. 'xXx' (2002)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/I-nV09WiCYd-3GQNgpa09oYEEe4=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924507/1_vYwuFRhl_-pHHbI1t4USPQ.0.jpeg"
+    },
+    {
+      title: "14. 'No Holds Barred' (1989)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/6l0ZGbDN8qxyM-z7J--6vA8zuaI=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924509/1_4eb82cJTgwK0Fy1JJ3ZNDQ.0.jpeg"
+    },
+    {
+      title: "13. 'Showgirls' (1995)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/_36T6hvpGLnzRy-yi4wuDEGMrRg=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924511/1_3YoxXCsJ4FgOLcs4lUVQMw.0.jpeg"
+    },
+    {
+      title: "12. 'She's All That' (1999)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/ATsSevO6gZjVUPKBmfwYrN-0XFs=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924513/1_-Pv7xhzYm9LXtIjf7QuQww.0.jpeg"
+    },
+    {
+      title: "11. 'The Beach' (2000)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/iPXJxib7vXDIkCxrMaBdBmDbRmk=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924515/1_Qux-atiCVl16-T_NAR0MYQ.0.jpeg"
+    },
+    {
+      title: "10. 'Speed 2: Cruise Control' (1997)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/4bwlUjK8lhDFaaN1uAXLS4XgDe0=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924517/1_z_5VWJGJw_eO184BE_aF2Q.0.jpeg"
+    },
+    {
+      title: "9. 'Over the Top' (1987)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/jIbUwn1f2j2FdYSX_iE9T2YNY9A=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924519/1_PC8rfJlKQ6ptbJLJoQEDvQ.0.jpeg"
+    },
+    {
+      title: "8. 'Final Destination' (2000)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/g_62e8hgV4v41UOXRdfMEd_T3W4=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924521/1_j6GwEJrqooq8AxJLcq-bWg.0.jpeg"
+    },
+    {
+      title: "7. 'Anaconda' (1997)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/0f_MfvQOFXJylWDJm-slMrdnVAo=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924523/1_WIDIXTBSsRu62joTNX1XzA.0.jpeg"
+    },
+    {
+      title: "6. 'White Chicks' (2004)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/F3hUoccYMakezYMXYNVyhDnJeUw=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924525/1_6qDP8NOCfuJssepbYwJNhA.0.jpeg"
+    },
+    {
+      title: "5. 'Batman & Robin' (1997)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/FZcqEL8V855FKB4M6xDB38TAk6Y=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924527/1_DuJulNqNDcVUcvHKyz7qag.0.jpeg"
+    },
+    {
+      title: "4. 'Masters of the Universe' (1987)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/FM5UwbMbYmnJuK65ZcMVhTX6R-8=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924529/1_KGwFf2kh9wMqXBumFKpkIA.0.jpeg"
+    },
+    {
+      title: "3. 'Congo' (1995)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/T27BfC4Y591O81f9_dHdB2W7XXM=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924531/1_nY7zCd3rus8-hRjUWtLeMQ.0.jpeg"
+    },
+    {
+      title: "2. 'Wild Wild West' (1999)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/0ZYicEAwUF6D0SthN6OOfSri6ew=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924535/1_vfopevJA2WEjQd-hDOYsog.0.jpeg"
+    },
+    {
+      title: "1. 'Godzilla' (1998)",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/cpk5N4qy8WrP0rTB8mNZqdHxzGo=/0x0:2500x400/1200x0/filters:focal(0x0:2500x400):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/8924537/1_hdgFfLLtrozbLZdl1tMzhA.0.jpeg"
+    }
+  ];
+
+  return res.end(JSON.stringify(movies));
+}
